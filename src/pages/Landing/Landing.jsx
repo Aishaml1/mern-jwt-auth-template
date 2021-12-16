f806818a8ae2d66fad2d6bb2f781c321bd66736a
Mandy_Mascetti <armascetti@gmail.com>
2021-12-16 11:57:41 -0500

    landing page add

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = ({ user }) => {
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
       <h1> Welcome to the Homepage</h1>
+      <h1>Lets make Github blow upppppppppp</h1>
     </main>
   )
 }
